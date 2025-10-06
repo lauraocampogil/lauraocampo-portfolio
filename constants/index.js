7de887b0f9a510c4fabda0e0feb3fed8c8a87cb1
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2025-10-06 21:15:57 +0200

    fix error in link with aboutme

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,6 @@
 export const navLinks = [
 	{ name: "Home", id: "./" },
-	{ name: "About me", id: "./about" },
+	{ name: "About me", id: "#about" },
 	{ name: "Skills", id: "#skills" },
 	{ name: "Projects", id: "#projects" },
 	{ name: "Objective", id: "#objective" },
